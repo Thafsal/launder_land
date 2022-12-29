b973e3932da5f1f06a3cf18fbea97946d24a9f64
Thafsal <thafsalthafsu@gmail.com>
2022-12-29 17:19:32 +0530

    added combo section

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsStars} from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { pdata } from "./pdata";
+import "./Popular.css";
 
 const Popular = () => {
   return (
-    <div>Popular</div>
+    <section className="popular">
+      <div className="container popular__container">
+        <div className="section__header">
+          <span>
+            <BsStars/>
+          </span>
+          <h2>COMBO OFFERS</h2>
+        </div>
+        <div className="popular__wrapper">
+          {pdata.map(({ id, name,price, path, imge }) => {
+            return (
+              <div className="cards popular__program" key={id}>
+                <h3>{name}</h3>
+                <span><img src={imge} alt={name} /></span>
+                <small>{price}</small>
+                <Link to={path} className="btn sm">
+                  ADD TO CART <AiOutlinePlus />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/Popular/pdata.js
@@ -0,0 +1,27 @@
+import spcombo from '../../images/sp.jpg'
+import bpcombo from '../../images/bp.jpg'
+import dpcombo from '../../images/dp.jpg'
+
+export const pdata = [
+    {
+        id:1,
+        name:"SHIRTS AND PANTS COMBO",
+        price: "20 Rs",
+        imge :spcombo,
+        path : '/products'
+    },
+    {
+        id:2,
+        name:"BLANKETS PILLOW COVER COMBO",
+        price: "50 Rs",
+        imge :bpcombo,
+        path : '/products'
+    },
+    {
+        id:3,
+        name:"DENIM JEANS X2 COMBO",
+        price: "26 Rs",
+        imge :dpcombo,
+        path : '/products'
+    },
+]
