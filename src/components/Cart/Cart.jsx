57fd22506e72fb512ce86c561e913dbe1ec438cc
Thafsal <thafsalthafsu@gmail.com>
2023-01-04 14:56:34 +0530

    added pricing sections

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import './Cart.css';
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <section>
+      <div className="cart-Container">
+        <div className="Header">
+          <h3 className="Heading">Shopping Cart</h3>
+          <h5 className="Action">Remove all</h5>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Cart
+export default Cart;
