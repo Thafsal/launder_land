96177bc17c304226f772828a4eaaa664e8f148eb
Thafsal <thafsalthafsu@gmail.com>
2023-01-10 23:47:30 +0530

    added env and commerce lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Header from './components/Header/Header'
 import Routing from './components/Routing/Routing'
 import Footer from './components/Footer/FooterPage'
-
+import { commerce } from '././lib/commerce';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products,setProducts] = useState([]);
+
+  const fetchProducts = async ()=> {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+
+    fetchProducts();
+
+  },[])
+
+  console.log(products);
+  
   return (
     <div className="App">
      <Header/>
