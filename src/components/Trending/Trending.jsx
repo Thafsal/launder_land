a1c25dc25040ca24e3e351e0ba3400b9d0572472
Thafsal <thafsalthafsu@gmail.com>
2023-01-19 15:40:32 +0530

    all section made responsive

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -3,14 +3,13 @@ import { AiOutlineFire, AiOutlinePlus } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { tdata } from "./tdata";
 import "./Trending.css";
-import { useContext } from "react";
-import { MyProvider } from "../../App";
+// import { useContext } from "react";
+// import { MyProvider } from "../../App";
 
 const Trending = () => {
 
-  const data = useContext(MyProvider);
-  console.log(data);
   return (
+    
     <section className="programs">
       <div className="container programs__container">
         <div className="section__header">
@@ -20,13 +19,13 @@ const Trending = () => {
           <h2>TRENDING PRODUCTS</h2>
         </div>
         <div className="programs__wrapper">
-          {tdata.map(({ id, name, offer, price, path, imge }) => {
+          {tdata.map(({ id,name,price,offer,imge,path }) => {
             return (
               <div className="card programs__program" key={id}>
                 <h3>{name}</h3>
-                <h5>{offer}</h5>
-                <span><img src={imge} alt={name} /></span>
-                <h4>{price}</h4>
+                <h5>{price}</h5>
+                <span><img src={imge} alt={id} /></span>
+                <h4>{offer}</h4>
                 <Link to={path} className="btn sm">
                   Learn More <AiOutlinePlus />
                 </Link>
