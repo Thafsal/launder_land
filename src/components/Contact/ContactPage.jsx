3c2cb29e90ab96ea4fb4fb0aa741e61585ad10eb
Thafsal <thafsalthafsu@gmail.com>
2023-02-28 12:59:10 +0530

    added contact page section code

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const ContactPage = () => {
+function ContactPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    // Send the form data to your backend or email service
+  }
+
   return (
-    <div>ContactPage</div>
-  )
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          required
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default ContactPage
+export default ContactPage;
